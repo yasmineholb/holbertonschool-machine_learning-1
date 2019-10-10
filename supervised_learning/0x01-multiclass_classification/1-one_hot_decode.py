14f3658443757d6ef72f1b6efc465dffbec4b3d6
Sidney Burkett <sidneyriffic@gmail.com>
2019-10-10 13:04:31 -0700

    Check input one hot dimension count in 0x02-1

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """Decode a one hot matrix to a numeric vector"""
-    if type(one_hot) is not np.ndarray:
+    if type(one_hot) is not np.ndarray or len(one_hot.shape) != 2:
         return None
     try:
         return np.argmax(one_hot, axis=0)
