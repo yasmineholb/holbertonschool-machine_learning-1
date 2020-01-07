12ea43b05d7c869645fee935121a104cbdb2576a
Sidney Burkett <sidneyriffic@gmail.com>
2020-01-07 01:34:19 -0800

    Add files for math 0x06-multivariate_prob

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -9,7 +9,7 @@ def correlation(C):
     """Calculate a correlation matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if C.shape[0] != C.shape[1]:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
     corrmatrix = np.ndarray(C.shape)
     for row in range(C.shape[0]):
